4226b9e2f300ca83c912ee6557665dce8d23cf91
Aleksey993 <drozd.aleksey@bk.ru>
2015-09-22 19:33:43 +0300

    Реализовал захват фото

--- WhereYouWatch/WhereYouWatch/MainForm.Designer.cs
+++ WhereYouWatch/WhereYouWatch/MainForm.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.MakePhotoButton = new System.Windows.Forms.Button();
+            this.mainPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +43,43 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Миша лох, как подключишься обызвай Альбину";
             // 
-            // Form1
+            // MakePhotoButton
+            // 
+            this.MakePhotoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MakePhotoButton.Location = new System.Drawing.Point(12, 287);
+            this.MakePhotoButton.Name = "MakePhotoButton";
+            this.MakePhotoButton.Size = new System.Drawing.Size(88, 23);
+            this.MakePhotoButton.TabIndex = 3;
+            this.MakePhotoButton.Text = "Сделать фото";
+            this.MakePhotoButton.UseVisualStyleBackColor = true;
+            this.MakePhotoButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainPicture
+            // 
+            this.mainPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPicture.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.mainPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.mainPicture.Location = new System.Drawing.Point(12, 29);
+            this.mainPicture.Name = "mainPicture";
+            this.mainPicture.Size = new System.Drawing.Size(437, 252);
+            this.mainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mainPicture.TabIndex = 4;
+            this.mainPicture.TabStop = false;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(461, 322);
+            this.Controls.Add(this.mainPicture);
+            this.Controls.Add(this.MakePhotoButton);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "WhereYouWatch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +88,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MakePhotoButton;
+        private System.Windows.Forms.PictureBox mainPicture;
     }
 }
 
--- WhereYouWatch/WhereYouWatch/MainForm.cs
+++ WhereYouWatch/WhereYouWatch/MainForm.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
 
 namespace WhereYouWatch
 {
     public partial class MainForm : Form
     {
+        FilterInfoCollection videoDevices;
+        VideoCaptureDevice videoSource;
+        Image newImage;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = new VideoCaptureDevice();
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.NewFrame += videoSource_NewFrame;
+                videoSource.Start();
+                while (newImage == null){}
+                videoSource.Stop();
+                newImage = null;
+        }
+
+        void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            newImage= (Bitmap)eventArgs.Frame.Clone();
+            mainPicture.Image = newImage;
         }
     }
 }
